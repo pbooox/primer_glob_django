37e1a729436788cd2af99e08d5f38c60cebf8be3
pedro <cajaspedro@hotmail.com>
2019-09-09 12:54:47 -0600

    crea una nueva publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_pub, name='post_list'),
+    path('pub/<int:pk>/', views.detalle_pub, name='detalle_pub'),
+    path('pub/nueva', views.nueva_pub, name='nueva_pub'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
+from .forms import FormPub
 
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pubs': pubs})
 
-# Create your views here.
+def detalle_pub(request, pk):
+    p = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'p': p})
+
+def nueva_pub(request):
+    if request.method == "POST":
+        formulario = FormPub(request.POST)
+        if formulario.is_valid():
+            pub = formulario.save(commit=False)
+            pub.autor = request.user
+            pub.fecha_publicacion = timezone.now()
+            pub.save()
+            return redirect('detalle_pub', pk=pub.pk)
+    else:
+        formulario = FormPub()
+    return render(request, 'blog/nueva_pub.html', {'formulario': formulario})
+
